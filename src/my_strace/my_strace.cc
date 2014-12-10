d1d0b7d513e6856979d4cb7d53c42bbc00bb23f6
Adrien Garin <0x2adr1@gmail.com>
2014-12-10 16:47:31 +0100

    update my_strace

--- src/my_strace/my_strace.cc
+++ src/my_strace/my_strace.cc
@@ -1,27 +1,47 @@
 #include <sys/types.h> // pid_t
 #include <sys/ptrace.h> // ptrace()
-#include <cstdlib> // exit()
+#include <sys/wait.h> // waitpid()
+#include <sys/reg.h> // ORIG_EAX, EAX
+#include <sys/user.h>
+#include <cstdlib> // std::exit()
+#include <cstdio> // printf
+#include <iostream> // std::cerr
 
 #include <unistd.h> // fork()
 
 #include "my_strace.hh"
 
-static void handle_syscall(pid_t pid_child)
-{
-
-}
-
 static void trace_child(pid_t pid_child)
 {
     int status = 0;
-    waitpid(pid_child, &status, 0);
-    ptrace(PTRACE_SETOPTIONS, pid_child, 0, PTRACE_O_TRACESYSGOOD);
+    struct user_regs_struct user_regs;
+    bool enter_syscall = true;
 
-    while (handle_syscall(pid_child))
+    // our child tell us that he is starting execution
+    wait(&status);
+
+    while (true)
     {
-        //PTRACE_PEEKUSER allow us to read child registers
-        int syscall = ptrace(PTRACE_PEEKUSER, pid_child,
-                sizeof (long) * ORIG_EAX);
+        ptrace(PTRACE_SYSCALL, pid_child, 0, 0);
+        // this time he tell us that he enters in a syscall
+        wait(&status);
+
+        if (WIFEXITED(status))
+            break;
+
+        ptrace(PTRACE_GETREGS, pid_child, 0, &user_regs);
+
+        if (enter_syscall)
+        {
+            // TODO: print syscall name
+            enter_syscall = false;
+        }
+
+        else
+        {
+            std::cout << user_regs.orig_rax << std::endl;
+            enter_syscall = true;
+        }
     }
 }
 
@@ -37,19 +57,20 @@ void my_strace(const std::string& bin_path, char *argv[])
 
     else if (pid_child == 0)
     {
-        ptrace(PT_TRACE_ME);
+        ptrace(PTRACE_TRACEME);
         pid_child = getpid();
-        kill(pid_child, SIGSTOP);
 
-        const char *bin_argv[] =
+        char* const bin_argv[] =
         {
-            bin_path.c_str(),
+            const_cast<char*>(bin_path.c_str()),
             NULL
         };
 
-        return execvp(bin_path.c_str(), bin_argv);
+        execvp(bin_path.c_str(), bin_argv);
     }
 
     else
         trace_child(pid_child);
+
+    (void) argv;
 }
