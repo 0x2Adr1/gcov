01c20151f9fec3e0ffaf0d7fae82a76d4cf636d9
Adrien Garin <0x2adr1@gmail.com>
2014-12-13 21:14:41 +0100

    Handle --level3

--- src/main.cc
+++ src/main.cc
@@ -3,10 +3,11 @@
 
 #include "my_strace/my_strace.hh"
 #include "my_sscov/my_sscov.hh"
+#include "my_addr2line/my_addr2line.hh"
 
 static void usage()
 {
-    std::cout << "Usage: ./my_gcov --level{1,2,3,4} /path/to/binary [args]";
+    std::cout << "Usage: ./my_gcov --level{1|2|3|4} /path/to/binary [args]";
     std::cout << std::endl;
 
     std::exit(0);
@@ -29,6 +30,9 @@ int main(int argc, char *argv[])
 
         my_sscov(argv);
     }
+
+    else if (level_str == "--level3")
+        my_addr2line(argv);
 
     else
         std::cout << "Not implemented yet, sorry." << std::endl;
